16c3ca82a238a1a424ef291dcac71023d1808b76
Thiago Moura <waatsaaynew@gmail.com>
2022-11-22 21:15:40 -0300

    fix the exercise 3 and add 4

--- 3/3.ino
+++ 3/3.ino
@@ -6,33 +6,12 @@ int green2 = 6;
 int red2 = 7;
 
 void setup(){
-    pinMode(green, OUTPUT);
-    pinMode(yellow, OUTPUT);
-    pinMode(red, OUTPUT);
-    pinMode(green2, OUTPUT);
-    pinMode(red2, OUTPUT);
+    pinMode(3, OUTPUT);
 }
 
 void loop(){
-    digitalWrite(green, HIGH);
-    digitalWrite(red2, HIGH);
-    delay(4000);
-    
-    digitalWrite(green, LOW);
-    digitalWrite(yellow, HIGH);
-    delay(2000);
-    
-    digitalWrite(yellow, LOW);
-    digitalWrite(red, HIGH);
-    digitalWrite(red2, LOW);    
-    digitalWrite(green2, HIGH);
-    delay(3000);
-    digitalWrite(green2, LOW);
-    digitalWrite(red2, HIGH);
-    delay(200);
-    digitalWrite(red2, LOW);
-    delay(200);
-    digitalWrite(red2, HIGH);
-    digitalWrite(red, LOW);
-    delay(2000);
+    digitalWrite(3, HIGH);
+    delay(1000);
+    digitalWrite(3, LOW);
+    delay(1000);
 }
